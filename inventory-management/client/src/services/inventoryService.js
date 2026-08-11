ec578181c18ce3ae791157a70764095783ea4c02
rextitus <titusjr018@gmail.com>
2026-08-11 22:23:28 +0530

    Remove non-movie project folders from repository

--- inventory-management/client/src/services/inventoryService.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const stockIn = (data) => api.post('/inventory/stock-in', data);
-export const stockOut = (data) => api.post('/inventory/stock-out', data);
-export const getTransactions = (params) => api.get('/inventory/history', { params });
-export const getDashboardStats = () => api.get('/inventory/stats');
